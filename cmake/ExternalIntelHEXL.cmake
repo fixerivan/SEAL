1d11b9be3775b07d24b98eecb583d590dfd35fd9
fixerivan <ivan@fixer.tech>
2022-11-24 21:51:12 +0100

    Update ExternalIntelHEXL.cmake

--- cmake/ExternalIntelHEXL.cmake
+++ cmake/ExternalIntelHEXL.cmake
@@ -9,7 +9,7 @@ FetchContent_Declare(
 )
 FetchContent_GetProperties(hexl)
 
-set(INTEL_HEXL_HINT_DIR ${CMAKE_INSTALL_PREFIX}/lib/cmake)
+set(INTEL_HEXL_HINT_DIR ~/hexl-fpga-install/lib/cmake)
 
 if(NOT hexl_POPULATED)
     FetchContent_Populate(hexl)
